4b4d76eb1aea637914a4b6a2f1a658455e37d875
AshishCKH <ashish.khandelwal@trigyn.com>
2019-08-12 16:05:03 +0530

    Branch 2 created as branch-I not working

--- Code/DB.cs
+++ Code/DB.cs
@@ -9,21 +9,21 @@ namespace XMLEditor.Code
    {
       private static string strConn = ConfigurationManager.ConnectionStrings["Conn"].ToString();
      
-      public static DataSet GetByID(int ID)
-      {
+      //public static DataSet GetByID(int ID)
+      //{
 
-         DataSet ds = new DataSet();
-         object[] parameters;
-         Hashtable ht = new Hashtable(0);
-         ht.Add("@ID", ID);
-         parameters = SqlHelperParameterCache.SetParameter(strConn, "XMLFilesDetailGetByID", ht);
-         SqlHelper.FillDataset(strConn, "XMLFilesDetailGetByID", ds, null, parameters);
+      //   DataSet ds = new DataSet();
+      //   object[] parameters;
+      //   Hashtable ht = new Hashtable(0);
+      //   ht.Add("@ID", ID);
+      //   parameters = SqlHelperParameterCache.SetParameter(strConn, "XMLFilesDetailGetByID", ht);
+      //   SqlHelper.FillDataset(strConn, "XMLFilesDetailGetByID", ds, null, parameters);
 
-         //parameters = SqlHelperParameterCache.SetParameter(strConn, "......", ht);
-         //SqlHelper.FillDataset(strConn, "......", ds, null, parameters);
+      //   //parameters = SqlHelperParameterCache.SetParameter(strConn, "......", ht);
+      //   //SqlHelper.FillDataset(strConn, "......", ds, null, parameters);
 
-         return ds;
-      }
+      //   return ds;
+      //}
       
    }
 }
